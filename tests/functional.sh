973ca0e881ef6de22a5074715e2d01229a44cfb7
ZuluPro <montheanthony@hotmail.com>
2016-02-27 01:17:39 -0500

    Improve functional test and add to CI

--- tests/functional.sh
+++ tests/functional.sh
@@ -2,6 +2,18 @@
 export DATABASE_URL="sqlite:///test-sqlite"
 export STORAGE="dbbackup.storage.filesystem_storage"
 export STORAGE_OPTIONS="location=/tmp/"
+
+
 python tests/runtests.py migrate
+
+echo 'import testapp; testapp.models.CharModel.objects.all().delete(); testapp.models.CharModel.objects.create(field="a")' | python tests/runtests.py shell
+
 python tests/runtests.py dbbackup
+echo 'import testapp; testapp.models.CharModel.objects.all().delete()' | python tests/runtests.py shell
+
 python tests/runtests.py dbrestore --noinput
+
+response=$(echo 'import testapp; assert 1 == testapp.models.CharModel.objects.count()' | python tests/runtests.py shell &> /dev/stdout)
+[[ ! "$response" == *"AssertionError"* ]]
+
+exit $?
